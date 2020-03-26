4a27ade583815e85d7604d9d1beca798559b9823
zap <soldierzx0705@gmail.com>
2020-03-26 15:53:35 +0800

    fix vaildPlatformAttributes bug http://10.9.173.136/SideProject/ZapLib/issues/22

--- ZapLib/Security/ValidPlatformAttribute.cs
+++ ZapLib/Security/ValidPlatformAttribute.cs
@@ -18,29 +18,25 @@ namespace ZapLib.Security
         /// <summary>
         /// Web API 進入 Action 會執行這個方法驗證
         /// </summary>
-        /// <param name="actionContext"></param>
+        /// <param name="actionContext">WebAPI HttpActionContext</param>
         public override void OnAuthorization(HttpActionContext actionContext)
         {
-            var header = actionContext.Request.Headers;
-            string Signature = getHeader(header, "Channel-Signature");
+            HttpRequestMessage Request = actionContext.Request;
+            var header = Request.Headers;
+            string content = Request.Content == null ? "" : Request.Content.ReadAsStringAsync().Result;
+            string OuterSignature = getHeader(header, "Channel-Signature");
             string IV = getHeader(header, "Channel-Iv");
             string Authorization = getHeader(header, "Channel-Authorization");
 
-            if (!string.IsNullOrWhiteSpace(Authorization))
-            {
-                if (Authorization == Const.GodKey) return;
-                Crypto crypto = new Crypto();
-                if (crypto.DESEncryption(Signature, IV) == Authorization) return;
-            }
+            if (IsVaild(content, IV, Authorization, OuterSignature)) return;
 
-            HttpRequestMessage Request = actionContext.Request;
             HttpStatusCode httpcode = HttpStatusCode.Unauthorized;
             HttpResponseMessage Response = Request.CreateResponse(httpcode);
             Response.Content = new StringContent(
                 JsonConvert.SerializeObject(new { error = "Request is not valid, please check [ValidPlatform]" }), Encoding.UTF8, "application/json");
             actionContext.Response = Response;
-
         }
+
         private string getHeader(HttpRequestHeaders header, string key)
         {
             try
@@ -53,5 +49,25 @@ namespace ZapLib.Security
             }
         }
 
+        /// <summary>
+        /// 內對內 API 驗證平台是否可被信任
+        /// </summary>
+        /// <param name="content">HTTP Request.Content 字串內容</param>
+        /// <param name="IV">加密種子</param>
+        /// <param name="Authorization">以 Signature + Iv + 系統金鑰 使用 DES 雜湊後的結果</param>
+        /// <param name="OuterSignature">簽章，將傳送的資料以 MD5 加密後的字串</param>
+        /// <returns>這個請求是否可被信任</returns>
+        public bool IsVaild(string content, string IV, string Authorization, string OuterSignature)
+        {
+            Crypto crypto = new Crypto();
+            string InnerSignature = crypto.Md5(content);
+
+            if (!string.IsNullOrWhiteSpace(Authorization))
+            {     
+                if (Authorization == Const.GodKey) return true;
+                if (crypto.DESEncryption(OuterSignature, IV) == Authorization && InnerSignature == OuterSignature) return true;
+            }
+            return false;
+        }
     }
 }
---
+++ ZapLibUnitTest/Security/ValidPlatformAttributeTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZapLib.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ZapLib.Security.Tests
+{
+    [TestClass()]
+    public class ValidPlatformAttributeTests
+    {
+        [TestMethod()]
+        public void IsVaildTest()
+        {
+            string content = JsonConvert.SerializeObject(new { name="Zap"});
+            Crypto crypto = new Crypto();
+            string OuterSignature = crypto.Md5(content);
+            string Authorization = crypto.DESEncryption(OuterSignature);
+            string IV = crypto.IV;
+
+
+            ValidPlatformAttribute attr = new ValidPlatformAttribute();
+
+            // 請求內容完全合法且沒遭竄改
+            Assert.IsTrue(attr.IsVaild(content, IV, Authorization, OuterSignature));
+
+            // 請求內容遭竄改
+            string fake_content = JsonConvert.SerializeObject(new { name = "Jack" });
+            Assert.IsFalse(attr.IsVaild(fake_content, IV, Authorization, OuterSignature));
+
+            // 請求內容遭到竄改但是使用上帝金鑰
+            string god_key = "GoD!";
+            Const.GodKey = god_key;
+            Assert.IsTrue(attr.IsVaild(fake_content, IV, god_key, OuterSignature));
+        }
+    }
+}
